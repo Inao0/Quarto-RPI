989eee2291d86c746bd34cc34e4276f43b130c30
Haran <ara.rahier@gmail.com>
2021-02-16 11:09:49 +0100

    :bug: Fix printButton definition

--- include/wiring_helper.h
+++ include/wiring_helper.h
@@ -2,7 +2,7 @@
 #define WIRING_HELPER_H
 #ifndef BUILD_PC
 void displayButtons();
-void printButton(int i, int j);
+void printButton(int *i, int *j);
 void scanButton(int i);
 int buttonHeldDown(int i);
 void activateButton(int rowPin, int colPin);
